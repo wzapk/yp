20bd076d9457be874dd033bba30d9801e4885ee1
wzapk <qhdtc5@gmail.com>
2016-11-30 19:20:41 +0800

    modified:   .gitignore 	modified:   README.md 	modified:   composer.json 	modified:   config/web.php 	modified:   controllers/MController.php 	modified:   messages/zh-CN/options.php 	modified:   models/Contents.php 	modified:   models/Teachers.php 	modified:   views/layouts/main.php 	modified:   views/layouts/mobile.php 	modified:   views/layouts/pc.php 	modified:   views/m/index.php 	modified:   views/system/index.php 	modified:   web/css/manage.css

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yiiboard_yellow_page',
+    'username' => 'yiiboard',
+    'password' => 'yiiboard_Wangqiang_758240',
+    'charset' => 'utf8',
+];
